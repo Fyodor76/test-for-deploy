ed661b5179131bb6319c0424db2e129ba8d71ebc
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-04-15 00:51:35 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer } from 'react-toastify';
+
+import { Footer } from './components/footer/Footer.tsx';
+import { Header } from './components/header/Header.tsx';
+import { Router } from './components/router/Router.tsx';
+import { toastConfig } from './const/toastConfig.ts';
+import { BoardProvider } from './context/BoardContext.tsx';
+import { TodoProvider } from './context/TodoContext.tsx';
+import './index.scss';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="app-container">
+            <BoardProvider>
+                <TodoProvider>
+                    <Header/>
+                    <Router/>
+                    <Footer/>
+                </TodoProvider>
+            </BoardProvider>
+            <ToastContainer {...toastConfig} />
+        </div>
+    );
 }
-
-export default App
+export default App;
