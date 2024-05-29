735a1a30483c240082875e678015d0d195f7f4c5
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-29 13:35:26 +0300

    добавлены стили

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,13 +1,26 @@
 import { useContext } from "react"
 import { Product } from "../../components/product/Product"
-import { ProductsContext } from "../../context/ProductsContext"
+import { ProductsContext, filterProducts } from "../../context/ProductsContext"
+import { Button } from "../../ui/Button/Button"
 
 
 export const Main = () => {
-    const {state} = useContext(ProductsContext)
+    const {state, dispatch} = useContext(ProductsContext)
+
+    const resetFilters = () => {
+        dispatch(filterProducts(""));
+    }
 
     return (
         <div className="main">
+            <div className="main-header">
+                <div>
+                    <h1>Добро пожаловать на главную страницу Wildberries!</h1>
+                </div>
+                <div className="reset-filters">
+                    <Button size="small" background="base" color="basic" onClick={resetFilters}>Сбросить фильтры</Button>
+                </div>
+            </div>
             <div className="container">
                 {state.filteredProducts.length > 0 ? state.filteredProducts?.map((p, i) => <Product key={i} product={p.row}/>) : null}
             </div>
