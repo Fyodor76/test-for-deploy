6def4e077006abc71277f926edef27076632e02c
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-06-07 16:03:57 +0300

    Добавлен loader

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,22 @@ import { Router } from './components/router/Router.tsx';
 import { toastConfig } from './const/toastConfig.ts';
 import './index.scss';
 import { ProductsProvider } from './context/ProductsContext.tsx';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Categories } from './api/Categories.ts';
 import { AuthProvider } from './context/AuthContext.tsx';
+import { Loader } from './components/loader/Loader.tsx';
   
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setIsLoading(true)
                 await Categories.fetchCategories();
+                setIsLoading(false)
             } catch (e) {
                 console.log(e);
             }
@@ -28,12 +33,13 @@ function App() {
     
 
     return (
-        <AuthProvider> {/* Оборачиваем все приложение в AuthProvider */}
+        <AuthProvider>
         <div className="app-container">
           <ProductsProvider>
             <Header />
             <Router />
             <Footer />
+            {isLoading &&<Loader/>}
           </ProductsProvider>
           <ToastContainer {...toastConfig} />
         </div>
---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Loader: React.FC = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="loader"></div>
+    </div>
+  );
+};
