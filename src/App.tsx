ddbffaf37ac630a7d510d2c26ccbc126874b30ec
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-06-07 19:12:13 +0300

    удалён лишний useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,12 @@ import { Header } from './components/header/Header.tsx';
 import { Router } from './components/router/Router.tsx';
 import { toastConfig } from './const/toastConfig.ts';
 import './index.scss';
-import { useEffect, useState } from 'react';
-import { Categories } from './api/Categories.ts';
 import { AuthProvider } from './context/AuthContext.tsx';
 import { Loader } from './components/loader/Loader.tsx';
 import { ProductsProvider } from './context/ProductContext.tsx';
   
 
 function App() {
-
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setIsLoading(true)
-                await Categories.fetchCategories();
-                setIsLoading(false)
-            } catch (e) {
-                console.log(e);
-            }
-        };
-
-        fetchData();
-    }, []);
 
     
 
@@ -39,7 +21,6 @@ function App() {
             <Header />
             <Router />
             <Footer />
-            {isLoading &&<Loader/>}
           <ToastContainer {...toastConfig} />
         </div>
           </ProductsProvider>
