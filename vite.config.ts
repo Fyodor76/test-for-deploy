171a0bef64f90500562ca405ebb96f66faf8f8d2
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-06-08 22:15:35 +0300

    попытка избежать ошибки 404 github

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-    ],
+    plugins: [react()],
     base: 'wb-front',
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    },
     server: {
-        port: 3001, // Укажите другой порт, например 3000
-      }
-});
+      port: 3001,
+    },
+  });
